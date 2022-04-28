4337fe9b37931a52b691c0d46f77d1306271532b
hmz007 <hmz007@gmail.com>
2022-04-28 10:55:06 +0800

    build: add script to quick build driver
    Signed-off-by: hmz007 <hmz007@gmail.com>

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+KDIR=$1
+true ${KDIR:=/opt/FriendlyARM/build/linux}
+true ${DEST:=/tmp/r5-modules}
+
+function r8125_build() {
+	echo "Kernel source: ${KDIR}"
+	make ARCH=arm64 CROSS_COMPILE=aarch64-linux- KERNELDIR=${KDIR}
+}
+
+function r8125_install() {
+	local KREL=$(cd ${KDIR} && make kernelrelease)
+
+	mkdir -p ${DEST}/lib/modules/${KREL}/extra
+	cp r8125.ko ${DEST}/lib/modules/${KREL}/extra/ -afv
+}
+
+r8125_build && r8125_install
+
